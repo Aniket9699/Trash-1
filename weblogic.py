14574ce322536538dab7e6b388a8a5e3e0cd95b2
Aniket9699 <89641646+Aniket9699@users.noreply.github.com>
2023-06-13 10:59:05 +0530

    Update weblogic.py

--- weblogic.py
+++ weblogic.py
@@ -5,7 +5,8 @@ echo "--------------------------------------------------"
 while IFS= read -r line; do
   URL=$(echo "$line" | cut -d',' -f1)
   MachineName=$(echo "$line" | cut -d',' -f2)
-  echo "URL = $URL and Machine Name = $MachineName"
+  Weblogic_Home_PAth=$(echo "$line" | cut -d',' -f3)
+  echo "URL = $URL and Machine Name = $MachineName Weblogic Home Path = $Weblogic_Home_PAth"
   # create start script
   cat /home/aniket/Oracle/products/Oracle_Home/oracle_common/common/bin/start_weblogic.py > /home/aniket/Start_$MachineName.py
   cat /home/aniket/Oracle/products/Oracle_Home/oracle_common/common/bin/get_node_by_node_name.py > /home/aniket/Stop_$MachineName.py
@@ -25,11 +26,20 @@ echo "--------------------------------------------------"
 while IFS= read -r line; do
   URL1=$(echo "$line" | cut -d',' -f1)
   MachineName1=$(echo "$line" | cut -d',' -f2)
+  Weblogic_Home_PAth1=$(echo "$line" | cut -d',' -f3)
   echo "-------------------------------------------------------------------------------------------------------------"
-  echo "URL = $URL1 and Machine Name = $MachineName1"
+  echo "URL = $URL1 and Machine Name = $MachineName1 and Weblogic Home Path = $Weblogic_Home_PAth1"
   echo "--------------------------------------------------------------------------------------------------------------"
-  sh /home/aniket/Oracle/products/Oracle_Home/oracle_common/common/bin/wlst.sh /home/aniket/Stop_$MachineName1.py
+  echo sh /home/aniket/Oracle/products/Oracle_Home/oracle_common/common/bin/wlst.sh /home/aniket/Stop_$MachineName1.py
   echo "--------------------------------------------------------------------------------------------------------------"
-  sh /home/aniket/Oracle/products/Oracle_Home/oracle_common/common/bin/wlst.sh /home/aniket/Start_$MachineName1.py
+  echo sh /home/aniket/Oracle/products/Oracle_Home/oracle_common/common/bin/wlst.sh /home/aniket/Start_$MachineName1.py
+  echo "-------------------------------------------------------------------------------------------------------------"
+  echo sh $Weblogic_Home_PAth1/stopWebLogic.sh
+  echo "-------------------------------------------------------------------------------------------------------------"
+  echo sh $Weblogic_Home_PAth1/startWebLogic.sh
+  echo "-------------------------------------------------------------------------------------------------------------"
+  echo sh $Weblogic_Home_PAth1/stopNodeManager.sh
+  echo "-------------------------------------------------------------------------------------------------------------"
+  echo sh $Weblogic_Home_PAth1/startNodeManager.sh
   echo "-------------------------------------------------------------------------------------------------------------"
 done < Weblogic.txt
